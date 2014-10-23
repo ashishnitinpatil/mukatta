6a19e379c329e6905a8665a261fc9b0b87be0927
Ashish Patil <ashishnitinpatil@gmail.com>
2014-10-23 17:32:19 +0530

    ~setup customization

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
+#!/usr/bin/python
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-     )
+setup(
+    name='Mu Katta',
+    version='1.0',
+    description='A katta for Mu Sigmans!',
+    author='Ashish Patil',
+    author_email='ashish.patil@mu-sigma.com',
+    url='http://www.python.org/sigs/distutils-sig/',
+    install_requires=['Django==1.7'],
+)
