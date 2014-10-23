9a0720cdd912dd52c64f1ab316da84bb12f622cf
Ashish Patil <ashishnitinpatil@gmail.com>
2014-10-24 00:11:17 +0530

    ++Work around for syncdb superuser
    Added MySQL-python to setup dependencies

--- wsgi/djkatta/apkatta/models.py
+++ wsgi/djkatta/apkatta/models.py
@@ -1,3 +1,30 @@
+#!/usr/bin/env python
 from django.db import models
+from django.conf import settings
+from django.contrib.auth import models as auth_models
+from django.db.models import signals
 
-# Create your models here.
+# Create our own root user automatically.
+
+def create_rootuser(app, created_models, verbosity, **kwargs):
+  if not settings.DEBUG:
+    return
+  try:
+    auth_models.User.objects.get(username='root')
+  except auth_models.User.DoesNotExist:
+    print '*' * 80
+    print 'Creating "root" user -- login: root, password: root'
+    print '*' * 80
+    assert auth_models.User.objects.create_superuser(
+        'root',
+        'ashishnitinpatil@gmail.com',
+        'root'
+    )
+  else:
+    print '"root" user already exists.'
+
+signals.post_syncdb.connect(
+    create_rootuser,
+    sender=auth_models,
+    dispatch_uid='common.models.create_rootuser'
+)
