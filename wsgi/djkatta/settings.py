5124b740774b20f708e169169790b3ae9d0893d7
Ashish Patil <ashishnitinpatil@gmail.com>
2014-11-02 17:05:47 +0530

    --coverage req

--- wsgi/djkatta/settings.py
+++ wsgi/djkatta/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'coverage',
+    # 'coverage',
     # 'django_nose',
     'bootstrap3',
     'djkatta.accounts',
