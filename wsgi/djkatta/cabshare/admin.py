1061c5af905e09399d213f1992bdf0e1c638b8a3
Ashish Patil <ashishnitinpatil@gmail.com>
2014-12-13 21:33:24 +0530

    ~cabshare owner display

--- wsgi/djkatta/cabshare/admin.py
+++ wsgi/djkatta/cabshare/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from djkatta.cabshare.models import cab_sharing
 
-# Register your models here.
+
+class CabShareAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'destination', 'travel_date',)
+    list_filter = ('travel_date', 'destination',)
+
+
+admin.site.register(cab_sharing, CabShareAdmin)
