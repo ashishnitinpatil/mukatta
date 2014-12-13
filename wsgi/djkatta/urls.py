eaf44b61d2c545208b939ed494f61b5aef5e90c5
Ashish Patil <ashishnitinpatil@gmail.com>
2014-12-13 19:34:18 +0530

    +robots.txt

--- wsgi/djkatta/urls.py
+++ wsgi/djkatta/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 
 urlpatterns = patterns('',
     # Django admin urls
@@ -14,10 +16,14 @@ urlpatterns = patterns('',
     # Home page - /home
     url(r"^about/$", "djkatta.views.about", name="about"),
 
+    # Robots page - /robots.txt
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt',
+        content_type='text/plain'), name="robots"),
+
     # "accounts" app urls
     url(r'^user/', include("djkatta.accounts.urls", namespace='user')),
 
     # "cabshare" app urls
     url(r'^cabshare/', include("djkatta.cabshare.urls", namespace='cabshare')),
-    url(r'^cabshare/$', 'djkatta.cabshare.views.index', name='index'),
+    url(r'^cabshare/$', 'djkatta.cabshare.views.index', name="index"),
 )
